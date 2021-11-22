a8a2798e969010d1a60aaf87231b7e8b00066076
Tellisa <91890378+Tellisa@users.noreply.github.com>
2021-11-22 10:56:41 +0000

    Create app.js

---
+++ Dog Years/app.js
@@ -0,0 +1,21 @@
+// Initialize myAge 
+const myAge=8;
+//Initialize earlyYears 
+let earlyYears = 2;
+earlyYears*=10.5;
+//Initialize laterYears 
+let laterYears=myAge-2;
+//Calculate number of dog years
+laterYears*=4
+// Display values
+console.log(`The earlyYears value is ${earlyYears} years. `);
+console.log(`The laterYears value is ${laterYears} years. `);
+//Combine earlyYears and laterYears
+let myAgeInDogYears =earlyYears+laterYears;
+//Initialize the myName 
+let myName='Tellisa'.toLowerCase();
+//Diplay
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years in dog years. `);
+
+
+
